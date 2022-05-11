c2a1059c9c9bcfe5d8ff5081261c0706c9af5fb6
Virag Kormoczy <vkormoczy@gmail.com>
2022-05-11 14:03:44 +0300

    Add other project objects

--- objects/other.js
+++ objects/other.js
@@ -1,3 +1,90 @@
-const otherProjects = [];
+const otherProjects = [
+  {
+    title: "Balance Sheet",
+    image: "../images/projects/balancesheet.png",
+    description:
+      "A sample balance sheet, showing a company's financial information. The user can jump between the 'Total' rows by clicking the 'tab' button.",
+    languagesTools: ["HTML", "CSS", "Flexbox"],
+    liveUrl: "https://virag-ky.github.io/Balance-Sheet/",
+    githubUrl: "https://github.com/virag-ky/Balance-Sheet",
+  },
+  {
+    title: "Cafe Menu",
+    image: "../images/projects/cafe-menu.png",
+    description:
+      "Cafe Menu project, inspired by freeCodeCamp's cafe menu lesson.",
+    languagesTools: ["HTML", "CSS", "Flexbox"],
+    liveUrl: "https://virag-ky.github.io/Mini-project-Coffee-Menu/",
+    githubUrl: "https://github.com/virag-ky/Mini-project-Coffee-Menu",
+  },
+  {
+    title: "Landing Page",
+    image: "../images/projects/electric_car-landing_page.png",
+    description:
+      "This is a landing page project from the Odin Project, the topic is about an electric car company.",
+    languagesTools: ["HTML", "CSS", "Flexbox"],
+    liveUrl: "https://virag-ky.github.io/Electric-car-landing-page/",
+    githubUrl: "https://github.com/virag-ky/Electric-car-landing-page",
+  },
+  {
+    title: "HTML-CSS Quiz",
+    image: "../images/projects/html_css-quiz.png",
+    description:
+      "Simple quiz about HTML and CSS. The user can choose between the answers.",
+    languagesTools: ["HTML", "CSS", "Flexbox"],
+    liveUrl: "https://virag-ky.github.io/HTML-CSS-Quiz/",
+    githubUrl: "https://github.com/virag-ky/HTML-CSS-Quiz",
+  },
+  {
+    title: "Magazine",
+    image: "../images/projects/magazine.png",
+    description: "This is a sample magazine project, made with Grid layout.",
+    languagesTools: ["HTML", "CSS", "Grid", "Flexbox"],
+    liveUrl: "https://virag-ky.github.io/Magazine/",
+    githubUrl: "https://github.com/virag-ky/Magazine",
+  },
+  {
+    title: "Nutrition Label",
+    image: "../images/projects/nutrition-label.png",
+    description: "Nutrition label project, inspired by freeCodeCamp's lesson.",
+    languagesTools: ["HTML", "CSS", "Flexbox"],
+    liveUrl: "https://virag-ky.github.io/Nutrition-Label/",
+    githubUrl: "https://github.com/virag-ky/Nutrition-Label",
+  },
+  {
+    title: "Odin Recipes",
+    image: "../images/projects/recipe-page.png",
+    description:
+      "Recipe website project from the Odin Project. The user can navigate between the homepage and 3 additional recipe pages.",
+    languagesTools: ["HTML", "CSS", "Flexbox"],
+    liveUrl: "https://virag-ky.github.io/Odin-Recipes/",
+    githubUrl: "https://github.com/virag-ky/Odin-Recipes",
+  },
+  {
+    title: "Registration Form",
+    image: "../images/projects/registration-form.png",
+    description:
+      "A simple registration form, inspired by freeCodeCamp's lesson.",
+    languagesTools: ["HTML", "CSS", "Flexbox"],
+    liveUrl: "https://virag-ky.github.io/Registration-Form/",
+    githubUrl: "https://github.com/virag-ky/Registration-Form",
+  },
+  {
+    title: "Tribute Page - Marilyn Monroe",
+    image: "../images/projects/tribute-page.png",
+    description: "Tribute page project of Marilyn Monroe.",
+    languagesTools: ["HTML", "CSS", "Flexbox"],
+    liveUrl: "https://virag-ky.github.io/Tribute-page--Marilyn_Monroe/",
+    githubUrl: "https://github.com/virag-ky/Tribute-page--Marilyn_Monroe",
+  },
+  {
+    title: "Technical Documentation Page",
+    image: "../images/projects/technical-documentation-page.png",
+    description: "JavaScript documentation page project.",
+    languagesTools: ["HTML", "CSS", "Flexbox"],
+    liveUrl: "https://virag-ky.github.io/Technical_documentation_page/",
+    githubUrl: "https://github.com/virag-ky/Technical_documentation_page",
+  },
+];
 
 export default otherProjects;
