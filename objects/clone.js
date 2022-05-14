e6b83ba49460d5ffe64296b5ec28aeee2277e497
Virag Kormoczy <vkormoczy@gmail.com>
2022-05-14 19:29:42 +0300

    Replace images

--- objects/clone.js
+++ objects/clone.js
@@ -1,32 +1,32 @@
 const cloneProjects = [
   {
-    title: 'Google homepage',
-    image: '../images/projects/google-homepage.png',
+    title: "Google homepage",
+    image: "../images/google-homepage.png",
     description:
-      'A static clone of the Google homepage, made with HTML and CSS. This was my very first project I built.',
-    languagesTools: ['HTML', 'CSS', 'Flexbox'],
-    liveUrl: 'https://virag-ky.github.io/Google-homepage/',
-    githubUrl: 'https://github.com/virag-ky/Google-homepage',
+      "A static clone of the Google homepage, made with HTML and CSS. This was my very first project I built.",
+    languagesTools: ["HTML", "CSS", "Flexbox"],
+    liveUrl: "https://virag-ky.github.io/Google-homepage/",
+    githubUrl: "https://github.com/virag-ky/Google-homepage",
     id: 2,
   },
   {
-    title: 'Youtube',
-    image: '../images/projects/youtube-clone.png',
+    title: "Youtube",
+    image: "../images/youtube-clone.png",
     description:
-      'A static Youtube clone project where I display some of my favorite music videos. The videos are embedded in HTML and the user can view them.',
-    languagesTools: ['HTML', 'CSS', 'Flexbox', 'Grid'],
-    liveUrl: 'https://virag-ky.github.io/Youtube-Clone/',
-    githubUrl: 'https://github.com/virag-ky/Youtube-Clone',
+      "A static Youtube clone project where I display some of my favorite music videos. The videos are embedded in HTML and the user can view them.",
+    languagesTools: ["HTML", "CSS", "Flexbox", "Grid"],
+    liveUrl: "https://virag-ky.github.io/Youtube-Clone/",
+    githubUrl: "https://github.com/virag-ky/Youtube-Clone",
     id: 3,
   },
   {
     title: "Virag's Web Development Course",
-    image: '../images/projects/capstone.png',
+    image: "../images/capstone.png",
     description:
       "This is my first capstone project from Microverse, the topic is about a web development course. The user can navigate between the 'homepage' and the 'about' page. The featured teachers section is made with JavaScript.",
-    languagesTools: ['HTML', 'Sass', 'JavaScript'],
-    liveUrl: 'https://virag-ky.github.io/Capstone-Web-Development-Course/',
-    githubUrl: 'https://github.com/virag-ky/Capstone-Web-Development-Course',
+    languagesTools: ["HTML", "Sass", "JavaScript"],
+    liveUrl: "https://virag-ky.github.io/Capstone-Web-Development-Course/",
+    githubUrl: "https://github.com/virag-ky/Capstone-Web-Development-Course",
     id: 4,
   },
 ];
