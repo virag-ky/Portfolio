77ac18321ea5df443e9e658571a4c9e01781a8fa
Virag Kormoczy <vkormoczy@gmail.com>
2022-05-25 21:00:29 +0300

    Add cocktailDB API project

--- objects/api-projects.js
+++ objects/api-projects.js
@@ -1,13 +1,23 @@
 const apiProjects = [
   {
-    title: 'Free-To-Play Games',
-    image: 'images/freetoplaygames.png',
+    title: "Free-To-Play Games",
+    image: "images/freetoplaygames.png",
     description:
-      'This project is based on an external API, called FreeToGame API. The project was built through pair programming (by me and my partner) and it was our JavaScript capstone project. The data of the games, which is fetched from the API, gets displayed on the cards (title, description, additional information). The user can leave a comment on the game cards and give it a like also by clicking the heart icon.',
-    languagesTools: ['HTML', 'Sass', 'JavaScript', 'API', 'Webpack'],
-    liveUrl: 'https://virag-ky.github.io/Free-To-Play-Games/',
-    githubUrl: 'https://github.com/virag-ky/Free-To-Play-Games',
+      "This project is based on an external API, called FreeToGame API. The project was built through pair programming (by me and my partner) and it was our JavaScript capstone project. The data of the games, which is fetched from the API, gets displayed on the cards (title, description, additional information). The user can leave a comment on the game cards and give it a like also by clicking the heart icon.",
+    languagesTools: ["HTML", "Sass", "JavaScript", "API", "Webpack"],
+    liveUrl: "https://virag-ky.github.io/Free-To-Play-Games/",
+    githubUrl: "https://github.com/virag-ky/Free-To-Play-Games",
     id: 1,
+  },
+  {
+    title: "Cocktails",
+    image: "images/cocktails.png",
+    description:
+      "This is a project about cocktails, the CocktailDB API is used to get the data. The user can choose from categories or type a cocktails name in the search and look at the details (instructions, ingredients).",
+    languagesTools: ["HTML", "Sass", "JavaScript", "API", "Grid", "Flexbox"],
+    liveUrl: "https://virag-ky.github.io/Cocktails/",
+    githubUrl: "https://github.com/virag-ky/Cocktails",
+    id: 2,
   },
 ];
 
